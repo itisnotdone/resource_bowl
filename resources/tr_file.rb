3a2792bc37999db6911195164c86eb33db797cdb
Don Draper <donoldfashioned@gmail.com>
2018-07-27 09:56:21 +0000

    add translating strings with regexp

--- resources/tr_file.rb
+++ resources/tr_file.rb
@@ -3,8 +3,25 @@
 resource_name :rb_tr_file
 
 property :source_file, String
-property :as_is, String
-property :to_be, String
+property :search_regexp, String
+property :string_to_be, String
 
 action :translate do
+  new_file_content = []
+  ::File.readlines(new_resource.source_file).each do |line|
+    if line =~ /#{new_resource.search_regexp}/
+      new_file_content.append(
+        line.gsub!(
+          /#{new_resource.search_regexp}/,
+          new_resource.string_to_be
+        )
+      )
+    else
+      new_file_content.append(line)
+    end
+  end
+
+  ::File.open(new_resource.source_file, 'w') do |file|
+    file.puts(new_file_content)
+  end
 end
